dbc0afbd70e194c1c451c12e2f8614f46e682796
jonahnathan0 <jan226@lehigh.edu>
2025-04-30 17:13:19 -0400

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -21,7 +21,7 @@ st.title('Market Reactions to Central Bank Communications')
 # Display image at the top
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image('assets/Banking-December-FOMC-announcement-live-blog.jpg', width=500)
+    st.image('assets/Banking-December-FOMC-announcement-live-blog.jpg', width=800)
 
 
 st.markdown('''
